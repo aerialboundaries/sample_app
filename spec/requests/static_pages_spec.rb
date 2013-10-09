b941716191d4cf12bc8bcb8e6603b66247e9a168
Masato Takatani <void@leto.eonet.ne.jp>
2013-10-08 21:46:21 -0500

    Chapter 5.2 finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,7 +51,7 @@ describe "Static pages" do
 
 	describe "Contact Page" do
 
-		it "should have the content'Contact'" do
+		it "should have the content 'Contact'" do
 			visit '/static_pages/contact'
 			expect(page).to have_content('Contact')
 		end
